3596a99478e8e9086ddf4e9cf45ff87180d5bb1f
Tokiosman80F <tokiosman96@gmail.com>
2024-01-03 19:59:02 +0600

    fetch data and showing data in  the data is completed

--- src/Pages/MyJob/MyJob.jsx
+++ src/Pages/MyJob/MyJob.jsx
@@ -1,8 +1,19 @@
-import React from "react";
-
+import React, { useContext, useEffect, useState } from "react";
+import AuthProvider, { AuthContext } from "../../Provider/AuthProvider";
+import { data } from "autoprefixer";
+import { GrUpdate } from "react-icons/gr";
+import { MdDeleteForever } from "react-icons/md";
 const MyJob = () => {
+  const { user } = useContext(AuthContext);
+  const [myjobs, setMyjob] = useState([]);
+  useEffect(() => {
+    fetch(`http://localhost:3000/myjob/${user?.email}`)
+      .then((response) => response.json())
+      .then((data) => setMyjob(data))
+      .catch((error) => console.error(error));
+  }, [user?.email]);
   return (
-    <div >
+    <div>
       <div className="overflow-x-auto">
         {/* 
 search bar 
@@ -18,13 +29,18 @@ table
 
 */}
         <div className="flex  items-center justify-center gap-2">
-          <input type="text" className="border-2 border-gray-500 rounded-md px-2 py-1" />
-          <button className="bg-green-500 px-6 py-2 rounded-md ">Search </button>
+          <input
+            type="text"
+            className="border-2 border-gray-500 rounded-md px-2 py-1"
+          />
+          <button className="bg-green-500 px-6 py-2 rounded-md text-white">
+            Search
+          </button>
         </div>
         <table className="table table-zebra">
           {/* head */}
           <thead>
-            <tr className="text-xl uppercase">
+            <tr className="text-xl uppercase ">
               <th>Sl</th>
               <th>Title</th>
               <th>category</th>
@@ -36,13 +52,26 @@ table
             </tr>
           </thead>
           <tbody>
-            {/* row 1 */}
-            <tr>
-              <th></th>
-              <td></td>
-              <td></td>
-              <td></td>
-            </tr>
+            {myjobs.map((myjob, index) => (
+              <tr key={myjob._id} >
+                <th>{index + 1}</th>
+                <td>{myjob.title}</td>
+                <td>{myjob.department}</td>
+                <td>$ {myjob.salary}</td>
+                <td>{myjob.jobType}</td>
+                <td>{myjob.date}</td>
+                <td>
+                  <button className="bg-blue-500 text-white p-3 rounded-md flex items-center gap-2 justify-center ">
+                    <span>Update</span> <GrUpdate />
+                  </button>
+                </td>
+                <td>
+                  <button className="bg-red-400 text-white p-3 rounded-md flex items-center gap-2 justify-center ">
+                    <span>Delete</span> <MdDeleteForever />
+                  </button>
+                </td>
+              </tr>
+            ))}
           </tbody>
         </table>
       </div>
